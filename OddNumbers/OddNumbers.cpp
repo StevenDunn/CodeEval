0d362c57b45fab50865e881bacf3064aad0d159f
Steven A. Dunn <stevendunn@gmail.com>
2013-07-20 11:36:50 -0500

    added odd numbers solution in C++

---
+++ OddNumbers/OddNumbers.cpp
@@ -0,0 +1,11 @@
+// Odd Numbers program written for CodeEval.com by Steven A. Dunn
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  for(int i = 1; i < 100; i=i+2)
+    std::cout << i << std::endl;
+
+  return 0;
+}
