95097029daeae22886e904edb007dee54692834a
Steven A. Dunn <stevendunn@gmail.com>
2013-07-24 21:48:38 -0500

    added decimal to binary solution in c++

---
+++ DecimalToBinary/DecimalToBinary.cpp
@@ -0,0 +1,41 @@
+// Decimal To Binary solution in C++ for CodeEval.com by Steven A. Dunn
+
+#include <bitset>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using std::bitset;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::string;
+
+int main(int argc, char* argv[])
+{
+	ifstream inputFile(argv[1]);
+	string line;
+	
+	if (inputFile)
+	{
+		while (getline(inputFile, line))
+		{
+			int n = atoi(line.c_str());
+			string binaryString = bitset<64>(n).to_string();
+			
+			int idx = 0;
+			for (int i = 0; i < binaryString.size(); ++i)
+				if (binaryString.at(i) == '1')
+				{
+					idx = i;
+					break;
+				}
+			binaryString = binaryString.substr(idx);	
+			
+			cout << binaryString << endl;	
+		}
+		inputFile.close();
+	}
+	return 0;
+}
