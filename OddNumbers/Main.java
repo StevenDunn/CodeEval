5a339f9526ce55f1b480e7a2d7ef440f7de8a12b
Steven A. Dunn <stevendunn@gmail.com>
2013-08-12 19:38:52 -0500

    added odd numbers solution in Java

---
+++ OddNumbers/Main.java
@@ -0,0 +1,10 @@
+// Odd Numbers solution in Java for CodeEval.com by Steven A. Dunn
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    for(int i = 1; i < 100; i = i + 2)
+      System.out.println(i);
+  }
+}
