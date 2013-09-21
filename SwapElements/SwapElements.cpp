84581867fcde40d27ebbe0c8c5e2e776e30bec6c
Steven A. Dunn <stevendunn@gmail.com>
2013-09-21 13:14:18 -0700

    final solution

--- SwapElements/SwapElements.cpp
+++ SwapElements/SwapElements.cpp
@@ -19,6 +19,10 @@ using std::string;
 using std::vector;
 
 vector<int> parseElements(string);
+vector<string> getTokens(string line);
+vector<string> getSwapTokens(string);
+vector<int> applySwaps(vector<int>, vector<string>);
+void printList(vector<int>);
 
 int main(int argc, char* argv[])
 {
@@ -33,7 +37,11 @@ int main(int argc, char* argv[])
       vector<int> elemVec = parseElements(elements);
 
       string swaps = line.substr(line.find(":") + 1);
-      cout << swaps << endl; 
+      vector<string> swapTokens = getSwapTokens(swaps);
+
+      vector<int> swappedList = applySwaps(elemVec, swapTokens);
+      
+      printList(swappedList);
     }
     inputFile.close();
   }
@@ -41,16 +49,62 @@ int main(int argc, char* argv[])
 
 vector<int> parseElements(string elements)
 {
-  vector<string> tokens;
-  istringstream iss(elements);
-  copy(
-    istream_iterator<string>(iss),
-    istream_iterator<string>(),
-    back_inserter<vector<string> >(tokens));
-
+   vector<string> tokens = getTokens(elements);
    vector<int> tokensAsNum;
    for(int i = 0; i < tokens.size(); ++i)
      tokensAsNum.push_back(atoi(tokens.at(i).c_str()));
 
    return tokensAsNum;
+}
+
+vector<string> getTokens(string line)
+{
+  vector<string> tokens;
+  istringstream iss(line);
+  copy(
+    istream_iterator<string>(iss),
+    istream_iterator<string>(),
+    back_inserter<vector<string> >(tokens));
+
+  return tokens;
+}
+
+vector<string> getSwapTokens(string swaps)
+{
+  vector<string> tokens = getTokens(swaps);
+ 
+  for(int i = 0; i < tokens.size(); ++i)
+  {
+    string token = tokens.at(i);
+    if(token.at(token.size()-1) == ',')
+    {
+      token = token.substr(0, token.size() - 1);
+      tokens.at(i) = token;
+    }
+  }
+
+  return tokens;
+}
+
+vector<int> applySwaps(vector<int> elements, vector<string> swaps)
+{
+  for(int i = 0; i < swaps.size(); ++i)
+  {
+    string token = swaps.at(i);
+    int elem1 = atoi(token.substr(0, token.find("-")).c_str());
+    int elem2 = atoi(token.substr(token.find("-") + 1).c_str());
+
+    int temp = elements.at(elem1);
+    elements.at(elem1) = elements.at(elem2);
+    elements.at(elem2) = temp;
+  }
+
+  return elements;
+} 
+
+void printList(vector<int> list)
+{
+  for(int i = 0; i < list.size(); ++i)
+    cout << list.at(i) << " ";
+  cout << endl;
 }
