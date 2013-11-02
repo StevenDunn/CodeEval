363d139a3d80d0b49fc07ed1226168b25032e59b
Steven A. Dunn <stevendunn@gmail.com>
2013-11-02 13:30:21 -0500

    working python 2 solution

---
+++ CapitalizeWords/cw.py
@@ -0,0 +1,10 @@
+# Capitalize Words solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = line.rstrip('\n').split(' ')
+  for word in line:
+    word = word[0].upper() + word[1:]
+    sys.stdout.write(word + " ")
+  print
