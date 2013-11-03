eb79f21263b608b3a2b0ecfaa0b8ab54756ecb02
Steven A. Dunn <stevendunn@gmail.com>
2013-11-02 19:35:39 -0500

    working python 2 solution

---
+++ JSONIds/jsonids.py
@@ -0,0 +1,26 @@
+# JSON Ids solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  ids = line.rstrip('\n').split("id")
+  ids = filter(None, ids)
+
+  if not ids: continue
+
+  sum = 0
+
+  for i in ids:
+    labels = i.split("label")
+
+    for j in labels:
+      start = j.find("Label") + 6;
+      j = j[start:]
+      end = j.find("\"")
+      j = j[:end]
+      try: 
+        j = int(j)
+        sum += j
+      except ValueError:
+        continue
+  print sum
