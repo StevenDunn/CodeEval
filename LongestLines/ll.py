0ffedc8a4fc7e73b70b0c8e5f495750129683289
Steven A. Dunn <stevendunn@gmail.com>
2013-11-03 13:01:16 -0600

    working python 2 solution

---
+++ LongestLines/ll.py
@@ -0,0 +1,14 @@
+# Longest Lines solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+N = int(f.readline())
+
+lines = []
+for line in f:
+  lines.append(line.rstrip('\n'))
+lines.sort(key=len, reverse=True)
+
+for i in range(0, N):
+  print lines[i]
