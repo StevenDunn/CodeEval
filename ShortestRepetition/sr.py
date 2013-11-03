22aa699c7b9710bb0af6928138ea46f6f228fc30
Steven A. Dunn <stevendunn@gmail.com>
2013-11-02 22:00:34 -0500

    working python 2 solution

---
+++ ShortestRepetition/sr.py
@@ -0,0 +1,14 @@
+# Shortest Repetition solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = line.rstrip('\n')
+  period = 1
+  subSeq = line[0]
+  for i in range(1, len(line)):
+    if not line[period:].find(subSeq) == 0:
+      period += 1
+      subSeq += line[i]
+
+  print period
