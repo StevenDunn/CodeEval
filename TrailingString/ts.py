4583bc1f5bfec8961311465a76f1413c607b7606
Steven A. Dunn <stevendunn@gmail.com>
2013-11-03 23:24:10 -0600

    working python 2 solution

---
+++ TrailingString/ts.py
@@ -0,0 +1,8 @@
+# Trailing String solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = line.rstrip('\n').split(',')
+  if line[0][-len(line[1]):] == line[1]: print 1
+  else: print 0
