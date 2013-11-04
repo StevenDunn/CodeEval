4308bf07d9d100cb0788e5d758c1e01984156374
Steven A. Dunn <stevendunn@gmail.com>
2013-11-04 15:02:40 -0600

    working python 2 solution

---
+++ JollyJumpers/jolly.py
@@ -0,0 +1,19 @@
+# Jolly Jumpers solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = map(int, line.rstrip('\n').split(' '))
+  N = line[0]
+  numbers = line[1:]
+
+  abs_diffs = 0
+  for i in range(0, N - 1):
+    abs_diffs += abs(numbers[i] - numbers[i + 1])
+
+  jj_sum = (N * (N - 1))/ 2
+
+  if abs_diffs == jj_sum:
+    print "Jolly"
+  else:
+    print "Not jolly"
