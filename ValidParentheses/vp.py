b0c387485ab7de720ee17183ebfcea7b4e30c83e
Steven A. Dunn <stevendunn@gmail.com>
2013-11-06 18:50:47 -0600

    working python 2 solution

---
+++ ValidParentheses/vp.py
@@ -0,0 +1,19 @@
+# Valid Parentheses solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = line.rstrip('\n')
+  parenStack = []
+  result = True
+  for c in line:
+    if c == '(': parenStack.append(')')
+    elif c == '[': parenStack.append(']')
+    elif c == '{': parenStack.append('}')
+    elif c == ')' or c == ']' or c == '}':
+      if parenStack: 
+        paren = parenStack.pop()
+        if not c == paren: result = False
+      else: result = False
+  if parenStack: result = False
+  print result
