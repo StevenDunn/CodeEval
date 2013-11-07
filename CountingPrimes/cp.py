69813ba7a7c2ad584e5aa631cc39422a3bf642c0
Steven A. Dunn <stevendunn@gmail.com>
2013-11-06 18:03:40 -0600

    working python 2 solution

---
+++ CountingPrimes/cp.py
@@ -0,0 +1,15 @@
+# Counting Primes solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+def isPrime(N):
+  for i in range(2, (N/2)+1):
+    if N % i == 0: return False
+  return True
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = line.rstrip('\n').split(',')
+  N, M, primeCount = int(line[0]), int(line[1]), 0
+  for i in range(N, M+1):
+    if isPrime(i): primeCount += 1
+  print primeCount
