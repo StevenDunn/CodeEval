4d26bdd23f968cdbe5956d2e8fc00acb203850eb
Steven A. Dunn <stevendunn@gmail.com>
2013-11-06 18:56:54 -0600

    working python 2 solution

---
+++ OverlappingRectangles/or.py
@@ -0,0 +1,17 @@
+# Overlapping Rectangles solution in Python 2 for CodeEval.com by Steven A. Dunn
+
+def isOverlap(a1, a2, b1, b2):
+  if a1 > a2: a1, a2 = a2, a1
+  if b1 > b2: b1, b2 = b2, b1
+  
+  overlap = False
+  for i in range(a1, a2+1):
+    if i >= b1 and i <= b2: overlap = True
+  return overlap
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = map(int, line.rstrip('\n').split(','))
+  if isOverlap(line[0], line[2], line[4], line[6]) and isOverlap(line[1], line[3], line[5], line[7]): print True
+  else: print False
