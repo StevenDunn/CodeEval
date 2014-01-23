c532eac091edd6fe294636e4a6bd2922b8461882
Steven A. Dunn <stevendunn@gmail.com>
2014-01-23 12:00:37 -0500

    working js soln

---
+++ ReverseWords/rw.js
@@ -0,0 +1,6 @@
+// Reverse Words soln in Node for CodeEval.com by Steven A. Dunn
+var fs = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function(line) {
+  if (line != "")
+    console.log(line.split(" ").reverse().join(" "));
+});
