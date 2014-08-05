5a9c05828e224ac480f2984f58b6cf8198bf727d
Steven A. Dunn <stevendunn@gmail.com>
2014-08-05 12:55:13 -0500

    working partial soln in rb

--- SimpleCalculator/calc.rb
+++ SimpleCalculator/calc.rb
@@ -1,12 +1,12 @@
 # simple calc soln in rb for codeeval by steven a dunn
 
-EXP_RGX = /(\-?\d+\.?\d*\s*)(\^){1}(\-?\s*\d+\.?\d*)/
+EXP_RGX = /(\-?\d+\.?\d*\s*)(\^){1}(\s*\-?\d+\.?\d*)/
 
-MLT_RGX = /(\-?\d+\.?\d*\s*)(\*){1}(\-?\s*\d+\.?\d*)/
-DIV_RGX = /(\-?\d+\.?\d*\s*)(\/){1}(\-?\s*\d+\.?\d*)/
+MLT_RGX = /(\-?\d+\.?\d*\s*)(\*){1}(\s*\-?\d+\.?\d*)/
+DIV_RGX = /(\-?\d+\.?\d*\s*)(\/){1}(\s*\-?\d+\.?\d*)/
 
-ADD_RGX = /(\-?\d+\.?\d*\s*)(\+){1}(\-?\s*\d+\.?\d*)/
-SUB_RGX = /(\-?\d+\.?\d*\s*)(\-){1}(\-?\s*\d+\.?\d*)/
+ADD_RGX = /(\-?\d+\.?\d*\s*)(\+){1}(\s*\-?\d+\.?\d*)/
+SUB_RGX = /(\-?\d+\.?\d*\s*)(\-){1}(\s*\-?\d+\.?\d*)/
 
 def resolve_parens(line)
     parens = []
@@ -32,45 +32,49 @@ def eval(line)
         updated_line = resolve_parens(line)
     end
     line = updated_line
-
+    line.gsub!(/\-\-/, "")
+#puts "line after paren resolution: #{line}"
     # resolve exps
     while line =~ EXP_RGX
         operand1, operator, operand2 = line.match(EXP_RGX).captures
-        result = ((operand1).to_f ** (operand2).to_f).to_s
-        line.gsub!(EXP_RGX, result)
+        result = ((operand1).to_f ** (operand2).to_f)
+        if result == Float::INFINITY
+            result = 200001
+        end
+        line.sub!(EXP_RGX, result.to_s)
     end
-
+#puts "line after exp: #{line}"
     # scan left to right resolving mults and divs
     line.split("").each do |char|
         if char == "*"
             operand1, operator, operand2 = line.match(MLT_RGX).captures
             result = ((operand1).to_f * (operand2).to_f).to_s
-            line.gsub!(MLT_RGX, result)
+            line.sub!(MLT_RGX, result)
         elsif char == "/"
             operand1, operator, operand2 = line.match(DIV_RGX).captures
             result = ((operand1).to_f / (operand2).to_f).to_s
-            line.gsub!(DIV_RGX, result)
+            line.sub!(DIV_RGX, result)
         end
     end
-
+#puts "line after mult/div: #{line}"
     # scan left to right resolving adds and subs
-    line.split("").each do |char|
+    line.split("").each_with_index do |char, index|
         if char == "+"
             operand1, operator, operand2 = line.match(ADD_RGX).captures
             result = ((operand1).to_f + (operand2).to_f).to_s
-            line.gsub!(ADD_RGX, result)
-        elsif char == "-" && line =~ SUB_RGX
+            line.sub!(ADD_RGX, result)
+        elsif char == "-" && index != 0 && line[index-1] == " " && line[index+1] == " "
             operand1, operator, operand2 = line.match(SUB_RGX).captures
             result = ((operand1).to_f - (operand2).to_f).to_s
-            line.gsub!(SUB_RGX, result)
+            line.sub!(SUB_RGX, result)
         end
     end
-
+#puts "line after add/sub: #{line}"
     line = resolve_parens(line)
 end
 
 File.open(ARGV[0], 'r').each_line do |line|
-    puts line
+    #puts line
     result = eval(line).to_f
     result == result.floor ? printf("%i\n", result.to_i) : printf("%.5f\n", result)
 
