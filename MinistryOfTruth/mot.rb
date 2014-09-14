35ecba23a2a8687ff75899907cec9eb00011d3f4
Steven A. Dunn <stevendunn@gmail.com>
2014-09-14 04:58:53 -0500

    working ministry of truth rb soln

--- MinistryOfTruth/mot.rb
+++ MinistryOfTruth/mot.rb
@@ -6,24 +6,68 @@ module Enumerable
     end
 end
 
-File.open(ARGV[0], 'r') do |file|
-    while line = file.gets
-        text, revision = line.split(";")
-        text.gsub!(/\s+/, ' ')
+def print_revised_text(text, indices, revision)
+    revised_text = ""
+    index = 0
+    while index < text.length
+        if indices.include?(index)
+            revised_text[index..(index + revision[0].length)] = revision[0]
+            index += revision[0].length
+            revision.delete_at(0)
+            next
+        elsif text[index] == " "
+            revised_text << " "
+        else
+            revised_text << "_"
+        end
+        index += 1
+    end
+    puts revised_text
+end
 
-        revision = revision.split
-        indices = []
-        revision.each do |word|
-            index = text.index(word)
-            if !index.nil?
-                indices << index
+File.open(ARGV[0], 'r').each_line do |line|
+    text, revision = line.split(";")
+    #puts line
+    text.gsub!(/\s+/, ' ')
+    split_text = text.split(" ")
+
+    revision = revision.split
+
+    indices = []
+    matched_words = []
+    invalid = nil
+    revision.each do |word|
+        index = text.index(word)
+        matches = split_text.select { |e| /#{word}/ =~ e }
+        #puts "#{matches}"
+        match = matches[0]
+        matches.each do |entry|
+            if indices.empty?
+                if !matched_words.include?(entry)
+                    match = entry
+                    index = text.index(word, text.index(match))
+                    break
+                end
+
+            elsif !matched_words.include?(entry) && text.index(entry) > indices[-1]
+                match = entry
+                index = text.index(word, text.index(match))
+                break
             end
         end
-        puts indices
-        if indices.nil? || !indices.sorted?
-            puts "I cannot fix history"
+
+        if !index.nil? && !matched_words.include?(match)
+            indices << index
+            matched_words << match
         else
-            puts "works"
+            invalid = true
+            break
         end
+    end
+
+    if !indices.sorted? || invalid
+        puts "I cannot fix history"
+    else
+        print_revised_text(text, indices, revision)
     end
 end
