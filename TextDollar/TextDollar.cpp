e94ea9b321354757d85757958779e0a5989dafaf
Steven A. Dunn <stevendunn@gmail.com>
2014-12-27 22:57:31 -0600

    working py and cpp text dollar solns

--- TextDollar/TextDollar.cpp
+++ TextDollar/TextDollar.cpp
@@ -1,6 +1,7 @@
 // Text Dollar solution for CodeEval.com in C++ by Steven A. Dunn
 
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -11,146 +12,204 @@ using std::endl;
 using std::ifstream;
 using std::ostringstream;
 using std::string;
+using std::to_string;
 
 string getDollarName(string);
-string getWord(char, int);
-string toWord(char);
-string getPositionWord(int);
-string getFinalWord(string);
+string getOnesWord(char);
+string convertTens(char, char);
+string tensWordTeens(char);
+string getTensWord(string);
+string getHundredsWord(string);
+string getThousandsWord(string);
+string getTenThousandsWord(string);
+string getHundredThousandsWord(string);
+string getMillionsWord(string);
+string getTenMillionsWord(string);
+string getHundredMillionsWord(string);
 
 int main(int argc, char* argv[]) {
 	ifstream inputFile(argv[1]);
 	string line;
-	
-	if(inputFile) 
+
+	if(inputFile)
 	{
 		while(getline(inputFile, line))
-		{
 			cout << getDollarName(line) << endl;
-		}
 		inputFile.close();
 	}
+	return 0;
 }
 
-string getDollarName(string numberStr)
+string getDollarName(string line)
 {
-	int length = numberStr.size();
-	string nextWord;
-	ostringstream oss("");
-	
-	for(int i = 0; i < length - 2; ++i)
-	{
-		char digit = numberStr.at(i);
-		if(digit == '0')
-			continue;
-		nextWord = getWord(digit, length - i);
-		oss << nextWord;
-	}
-	
-	if(length == 1)
-		nextWord = getFinalWord(numberStr.substr(0, 1));
-	else
-		nextWord = getFinalWord(numberStr.substr(length - 2, 2));
-		
-	oss << nextWord;
+	long number = atol(line.c_str());
+	string text = "";
 
-	oss << "Dollars";
-	return oss.str();
+	if (number < 100)
+		text += getTensWord(line);
+	else if (number < 1000)
+		text += getHundredsWord(line);
+	else if (number < 10000)
+		text += getThousandsWord(line);
+    else if (number < 100000)
+        text += getTenThousandsWord(line);
+    else if (number < 1000000)
+        text += getHundredThousandsWord(line);
+    else if (number < 10000000)
+        text += getMillionsWord(line);
+    else if (number < 100000000)
+        text += getTenMillionsWord(line);
+    else
+        text += getHundredMillionsWord(line);
+
+    text += "Dollars";
+
+   	return text;
 }
 
-string getWord(char digit, int position)
-{	
-	string word = toWord(digit) + getPositionWord(position);
-	return word;
-}
-
-string toWord(char digit)
+string getOnesWord(char digit)
 {
-	string result;
-	switch(digit)
+	int val = digit - '0';
+	switch(val)
 	{
-		case '1': result = "One"; break;
-		case '2': result = "Two"; break;
-		case '3': result = "Three"; break;
-		case '4': result = "Four"; break;
-		case '5': result = "Five"; break;
-		case '6': result = "Six"; break;
-		case '7': result = "Seven"; break;
-		case '8': result = "Eight"; break;
-		case '9': result = "Nine"; break;
+		case 1: return "One";
+		case 2: return "Two";
+		case 3: return "Three";
+		case 4: return "Four";
+		case 5: return "Five";
+		case 6: return "Six";
+		case 7: return "Seven";
+		case 8: return "Eight";
+		case 9: return "Nine";
+		case 0: return "";
 	}
-	return result;
+	return "";
 }
 
-string getPositionWord(int position)
+string convertTens(char firstDigit, char secondDigit)
 {
-	string result;
-	switch(position)
+	int val = firstDigit - '0';
+	switch(val)
 	{
-		case 10: result = "Billion"; break;
-		case 9: result = "HundredMillion"; break;
-		case 8: result = "TenMillion"; break;
-		case 7: result = "Million"; break;
-		case 6: result = "HundredThousand"; break;
-		case 5: result = "TenThousand"; break;
-		case 4: result = "Thousand"; break;
-		case 3: result = "Hundred"; break;
-		case 2: 
-		case 1:
-		default: result = "";
+		case 1: return tensWordTeens(secondDigit);
+		case 2: return "Twenty";
+		case 3: return "Thirty";
+		case 4: return "Forty";
+		case 5: return "Fifty";
+		case 6: return "Sixty";
+		case 7: return "Seventy";
+		case 8: return "Eighty";
+		case 9: return "Ninety";
+		case 0: return "";
 	}
-	return result;
+	return "";
 }
 
-string getFinalWord(string numberStr)
+string tensWordTeens(char digit)
 {
-	ostringstream oss("");
-	
-	char firstDigit = numberStr.at(0);
-	char lastDigit = numberStr.at(numberStr.size() - 1);
-	
-	// cout << numberStr << endl;
-	// 	cout << firstDigit << endl;
-	// 	cout << lastDigit << endl;
-	
-	if(numberStr.size() == 2) 
+	int val = digit - '0';
+	switch(val)
 	{
-		switch(firstDigit)
-		{
-			case '9': oss << "Ninety"; break;
-			case '8': oss << "Eighty"; break;
-			case '7': oss << "Seventy"; break;
-			case '6': oss << "Sixty"; break;
-			case '5': oss << "Fifty"; break;
-			case '4': oss << "Forty"; break;
-			case '3': oss << "Thirty"; break;
-			case '2': oss << "Twenty"; break;
-			case '1': 
-				{
-					switch(lastDigit)
-					{
-						case '0': oss << "Ten"; break;
-						case '1': oss << "Eleven"; break;
-						case '2': oss << "Twelve"; break;
-						case '3': oss << "Thirteen"; break;
-						case '4': oss << "Fourteen"; break;
-						case '5': oss << "Fifteen"; break;
-						case '6': oss << "Sixteen"; break;
-						case '7': oss << "Seventeen"; break;
-						case '8': oss << "Eighteen"; break;
-						case '9': oss << "Nineteen"; break;
-					}
-				}
-		}
+		case 0: return "Ten";
+		case 1: return "Eleven";
+		case 2: return "Twelve";
+		case 3: return "Thirteen";
+		case 4: return "Fourteen";
+		case 5: return "Fifteen";
+		case 6: return "Sixteen";
+		case 7: return "Seventeen";
+		case 8: return "Eighteen";
+		case 9: return "Nineteen";
 	}
-	if(numberStr.size() != 2 || firstDigit != 1)
-	{
-		oss << toWord(lastDigit);
-	}
-	
-	//cout << "last digit: " << lastDigit << endl;
-	//cout << numberStr.size() << endl;
+	return "";
+}
 
-	
-	return oss.str();
+string getTensWord(string text)
+{
+    int number = atoi(text.c_str());
+    string result = "";
+
+    if (number == 0)
+        return result;
+    else if (number < 10)
+    {
+        text = to_string(number);
+        result += getOnesWord(text[0]);
+    }
+    else if (number < 20)
+        result += convertTens(text[0], text[1]);
+    else
+    {
+        result += convertTens(text[0], text[1]);
+        string onesWord = getOnesWord(text[1]);
+        if (onesWord != "")
+            result += onesWord;
+    }
+    return result;
+}
+
+string getHundredsWord(string text)
+{
+	string result = getOnesWord(text[0]);
+	string sub = getTensWord(text.substr(1, 2));
+    if (result != "")
+        return result + "Hundred" + sub;
+    return sub;
+}
+
+string getThousandsWord(string text)
+{
+	string result = getOnesWord(text[0]);
+	string sub = getHundredsWord(text.substr(1));
+    if (result != "")
+        return result + "Thousand" + sub;
+    return sub;
+}
+
+string getTenThousandsWord(string text)
+{
+	string result = getTensWord(text.substr(0, 2));
+	string sub = getHundredsWord(text.substr(2));
+    if (result != "")
+        return result + "Thousand" + sub;
+    return sub;
+}
+
+string getHundredThousandsWord(string text)
+{
+	string result = getHundredsWord(text.substr(0, 4));
+	string sub = getHundredsWord(text.substr(3));
+    if (result != "")
+        return result + "Thousand" + sub;
+    return sub;
+}
+
+string getMillionsWord(string text)
+{
+	string result = getOnesWord(text[0]);
+	string sub = getHundredThousandsWord(text.substr(1));
+
+    if (result != "")
+        return result + "Million" + sub;
+    return sub;
+}
+
+string getTenMillionsWord(string text)
+{
+	string result = convertTens(text[0], text[1]);
+	string sub = getHundredThousandsWord(text.substr(2));
+
+    if (result != "")
+        return result + "Million" + sub;
+    return sub;
+}
+
+string getHundredMillionsWord(string text)
+{
+	string result = getHundredsWord(text.substr(0,4));
+	string sub = getHundredThousandsWord(text.substr(3));
+
+    if (result != "")
+        return result + "Million" + sub;
+    return sub;
 }
