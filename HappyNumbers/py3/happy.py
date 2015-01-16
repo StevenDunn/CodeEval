568528b9ff9a77686f73f0a3cc7fa08a6657a30b
Steven A. Dunn <stevendunn@gmail.com>
2015-01-16 01:18:54 -0600

    happy numbers py3 soln

---
+++ HappyNumbers/py3/happy.py
@@ -0,0 +1,20 @@
+# Happy Numbers solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+def isHappy(n, visited):
+  sum = 0
+  for c in n:
+    sum += int(c) * int(c)
+
+  if sum == 1: return True
+  if sum in visited: return False;
+  visited.append(sum)
+
+  if isHappy(str(sum), visited): return True;
+  return False;
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  visited = list()
+  if isHappy(line.rstrip('\n'), visited): print(1)
+  else: print(0)
