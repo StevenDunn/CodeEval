9b14d183cca94628ff1e0eb60a233a4a19559ed6
Steven A. Dunn <stevendunn@gmail.com>
2015-01-16 01:32:27 -0600

    armstrong numbers py3 soln

---
+++ ArmstrongNumbers/py3/armstrong.py
@@ -0,0 +1,15 @@
+# Armstrong Numbers solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+def isArmstrong(N):
+  sum = 0
+  numDigits = len(N)
+  for c in N:
+    sum += (int(c) ** numDigits)
+  if sum == int(N): return True
+  return False
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  if isArmstrong(line.rstrip('\n')): print(True)
+  else: print(False)
