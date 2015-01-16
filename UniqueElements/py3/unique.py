08ffd080951fd3d67e09392dc54eeaec29e41889
Steven A. Dunn <stevendunn@gmail.com>
2015-01-16 01:10:55 -0600

    unique elements py3 soln

---
+++ UniqueElements/py3/unique.py
@@ -0,0 +1,12 @@
+# Unique Elements solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  vals = line.rstrip('\n').split(',')
+  vals = map(int, vals)
+  unique = list(set(vals))
+  unique.sort()
+  for i in range(0, len(unique) - 1):
+    sys.stdout.write("%d," % unique[i]),
+  print (unique[-1])
