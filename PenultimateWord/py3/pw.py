699354d5f4fe5ea409cdcc226a9386b8bea178ed
Steven A. Dunn <stevendunn@gmail.com>
2015-01-16 01:37:11 -0600

    penultimate word py3 soln

---
+++ PenultimateWord/py3/pw.py
@@ -0,0 +1,7 @@
+# Penultimate Word solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = line.rstrip('\n').split(' ')
+  print (line[-2])
