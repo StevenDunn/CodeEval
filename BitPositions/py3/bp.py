37c0e993495a204928a0459d06ff0f44694493d2
Steven A. Dunn <stevendunn@gmail.com>
2015-01-16 00:57:55 -0600

    bit positions py3 soln

---
+++ BitPositions/py3/bp.py
@@ -0,0 +1,14 @@
+# Bit Positions solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+
+for line in f:
+  n, pos1, pos2 = line.split(',')
+  b = bin(int(n))
+  if b[-int(pos1)] == b[-int(pos2)]:
+    print ("true")
+  else:
+    print ("false")
+f.close()
