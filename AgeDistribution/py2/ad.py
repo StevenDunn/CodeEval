610d2cd5bed8d853a34ec64123f397643c49e55b
Steven A. Dunn <stevendunn@gmail.com>
2015-01-17 15:21:14 -0600

    age distribution py2 and py3 solns

---
+++ AgeDistribution/py2/ad.py
@@ -0,0 +1,28 @@
+# age distribution solution in py2 for codeeval.com by steven a dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+for line in f:
+    line = int(line.rstrip())
+    if line < 0:
+        print "This program is for humans"
+    elif line <= 2:
+        print "Still in Mama's arms"
+    elif line <= 4:
+        print "Preschool Maniac"
+    elif line <= 11:
+        print "Elementary school"
+    elif line <= 14:
+        print "Middle school"
+    elif line <= 18:
+        print "High school"
+    elif line <= 22:
+        print "College"
+    elif line <= 65:
+        print "Working for the man"
+    elif line <= 100:
+        print "The Golden Years"
+    else:
+        print "This program is for humans"
+f.close()
