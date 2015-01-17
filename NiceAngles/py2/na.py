c5cf71ffc48e5165dcf4f4ab333c223c70272138
Steven A. Dunn <stevendunn@gmail.com>
2015-01-17 14:59:54 -0600

    nice angles py2 and py3 solns

---
+++ NiceAngles/py2/na.py
@@ -0,0 +1,13 @@
+# nice angles soln in py2 for codeeval by steven a dunn
+
+import sys, math
+
+f = open(sys.argv[1], 'r')
+for line in f:
+    angle = float(line)
+    full_degree = math.floor(angle)
+    remaining_angle = angle - full_degree
+    minutes = math.floor(remaining_angle * 60)
+    seconds = math.floor(((remaining_angle * 60) - minutes) * 60)
+    sys.stdout.write("%d.%02d'%02d\"\n" % (full_degree, minutes, seconds))
+f.close()
