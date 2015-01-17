09af6bc1949b2fd2ee13ad5e03183a36ff1c6746
Steven A. Dunn <stevendunn@gmail.com>
2015-01-16 21:25:02 -0600

    slang flavor py3 soln

---
+++ SlangFlavor/py3/sf.py
@@ -0,0 +1,24 @@
+#slang flavor soln in py3 for codeeval by steven a dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+
+is_slang = False
+slang_index = 0
+slang = [", yeah!", ", this is crazy, I tell ya.", ", can U believe this?", ", eh?", ", aw yea.", ", yo.", "? No way!", ". Awesome!"]
+for line in f:
+    new_line = line
+    offset = 0
+    for index, c in enumerate(line):
+        if c == "." or c == "?" or c == "!":
+            if is_slang:
+                new_line = new_line[0:index+offset] + slang[slang_index] + new_line[index+offset+1:]
+                offset += len(slang[slang_index]) - 1
+                slang_index += 1
+                if slang_index >= 8:
+                    slang_index = 0
+            is_slang = not is_slang
+    new_line = new_line.rstrip()
+    print(new_line)
+f.close()
