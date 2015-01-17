bf3732ac4a78a42a13ef4dcb2c91ba741dd3139c
Steven A. Dunn <stevendunn@gmail.com>
2015-01-16 21:30:05 -0600

    read more py3 soln

---
+++ ReadMore/py3/rm.py
@@ -0,0 +1,18 @@
+# read more py3 soln for code eval by steven a dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+
+for line in f:
+    line = line.strip()
+    line_length = len(line)
+    if line_length > 55:
+        line = line[0:40]
+        last_space = line.rfind(" ")
+        if last_space != -1:
+            line = line[0:last_space]
+        line += "... <Read More>"
+    print(line)
+
+f.close()
