c0f2f39f90e4151d56e0fc98532a8b10d28e6446
Steven A. Dunn <stevendunn@gmail.com>
2015-01-17 18:27:09 -0600

    split the number py2 soln

---
+++ SplitTheNumber/py3/stn.py
@@ -0,0 +1,19 @@
+# Split the number soln in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  expr = line.rstrip('\n').split(' ')
+  expr[0] = list(expr[0])
+
+  for i in range(0, len(expr[1])):
+    if expr[1][i] == '+':
+      expr[0].insert(i, '+')
+    elif expr[1][i] == '-':
+      expr[0].insert(i, '-')
+    else:
+      continue
+
+  result = ''.join(expr[0])
+  print (result)
+  print (eval(result))
