272cc0ce6ff235d2cac0d31b2a05c0aba6c109d0
Steven A. Dunn <stevendunn@gmail.com>
2015-01-17 18:12:47 -0600

    road trip py2 and py3 soln

---
+++ RoadTrip/py3/rt.py
@@ -0,0 +1,24 @@
+# road trip soln in py2 for codeeval by steven a dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+for line in f:
+    line = line.rstrip().split(";")
+    distances = []
+    for entry in line:
+        if not entry:
+            continue
+        distance = int(entry.split(",")[1])
+        distances.append(distance)
+    distances.sort()
+
+    results = []
+    cur_pos = 0
+    for distance in distances:
+        location = distance - cur_pos
+        results.append(location)
+        cur_pos = distance
+    results = list(map(str, results))
+    print (",".join(results))
+f.close()
