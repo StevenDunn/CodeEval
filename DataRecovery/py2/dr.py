5dc6d7b324c4ddb93841dddf6b09d69c2f92c145
Steven A. Dunn <stevendunn@gmail.com>
2015-01-30 15:35:54 -0600

    working py2 data recovery soln

--- DataRecovery/py2/dr.py
+++ DataRecovery/py2/dr.py
@@ -2,14 +2,20 @@
 
 import sys
 
+def find_missing(indices, text_length):
+    i_sum = 0;
+    for i in indices:
+        i_sum += i
+    expected_sum = text_length * (text_length + 1) / 2;
+    return expected_sum - i_sum;
+
 f = open(sys.argv[1], 'r')
 for line in f:
-  line = line.rstrip().split(";")
-  text, order = line[0].split(), line[1].split()
-  mapping = zip(order, text)
-  print mapping
-  mapping.sort()
-  for entry in mapping:
-    print entry[1],
-  print
+    split_line = line.split(';')
+    text = split_line[0].split()
+    indices = map(int, split_line[1].split())
+    indices.append(find_missing(indices, len(text)))
+    for i in xrange(1, len(indices)+1):
+        print text[indices.index(i)],
+    print
 f.close()
