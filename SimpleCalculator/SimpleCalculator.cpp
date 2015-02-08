5361ec1a1a5ab45b8f970d0c49e0887734c6c97d
Steven A. Dunn <stevendunn@gmail.com>
2015-02-07 18:53:57 -0600

    98 percent soln to simple calc; can't figure out error cases but believe it to be subtle loss of precision errors

--- SimpleCalculator/SimpleCalculator.cpp
+++ SimpleCalculator/SimpleCalculator.cpp
@@ -1,10 +1,14 @@
 // Simple Calculator solution in C++ for CodeEval.com by Steven A. Dunn
 
+#include <cctype>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
+#include <functional>
+#include <iomanip>
 #include <iostream>
+#include <locale>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -24,25 +28,42 @@ bool isOperator(char);
 bool isLeftParen(char);
 bool isRightParen(char);
 string getSubExpr(string&);
-double updateResult(vector<string>, vector<string>);
+double updateResult(vector<string>&, vector<string>&);
 char getHighestPrecedenceOperator(vector<string>, int&);
 bool greaterOp(char, char);
 double apply(char, vector<string>, int);
 void updateState(vector<string>&, vector<string>&, int);
 string parse(string);
 
+template<typename T>
+void printVector(vector<T> vec);
+
+// trim from start
+static inline std::string &ltrim(std::string &s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s) {
+        return ltrim(rtrim(s));
+}
+
 int main(int argc, char* argv[])
 {
 	ifstream inputFile(argv[1]);
 	string line;
-	
+
 	if(inputFile)
 	{
 		while(getline(inputFile, line))
-    {
-      //cout << line << endl;
-      printLine(line);
-    }
+      		printLine(line);
 		inputFile.close();
 	}
 	return 0;
@@ -50,48 +71,53 @@ int main(int argc, char* argv[])
 
 void printLine(string line)
 {
-  ostringstream s;
+  	ostringstream s;
 	s.setf(ios::fixed);
-	s.precision(10);
+	s.precision(5);
 	s << evaluate(line);
-	cout << parse(s.str()) << endl;
+	cout << "###########final result: " << parse(s.str()) << endl;
 }
 
 double evaluate(string line)
 {
+	cout << "line is: " << line << endl;
 	string token;
 	string op;
-	
+
 	double result = 0.0;
-	
+
 	vector<string> operands;
 	vector<string> operators;
-	
+
 	while(line != "")
 	{
-    cout << line << endl;
-		token = parseNextToken(line);	
-
-		if(isOperator(token[0])) 
+		line = trim(line);
+		cout <<  "updated line is: " << line << endl;
+		token = parseNextToken(line);
+		cout << "token: " << token << " of length: " << token.size() << endl;
+		if(isOperator(token[0]))
 		{
 			if(token[0] == '-' && (operands.size() < operators.size() + 1))
 				token = "@";
-	
-			operators.push_back(token); 
+
+			operators.push_back(token);
 		}
-		else if(isLeftParen(token[0])) 
-		{ 
+		else if(isLeftParen(token[0]))
+		{
 			getSubExpr(line);
-      continue;
+      		continue;
 		}
-		else { operands.push_back(token); }
-    line = line.substr(token.size());
-    cout << line << endl;
+		else if(token != "")
+		{
+			operands.push_back(token);
+		}
+		if (token.size() > 0)
+    		line = line.substr(token.size());
+    	else
+    		line = line.substr(1);
 	}
-	
-	result = updateResult(operands, operators);
-cout << "result: " << result << endl;
-	return result;
+
+	return updateResult(operands, operators);
 }
 
 string parseNextToken(string& line)
@@ -99,14 +125,13 @@ string parseNextToken(string& line)
 	int idx = 0;
 	while(!isOperator(line[idx]) && !isLeftParen(line[idx]) && idx < line.size())
 		++idx;
-		
+
 	if(idx == 0)
 		++idx;
-	
-	string token = line.substr(0, idx);
-	//line = line.substr(idx);
-	
-	return token;
+
+	string result = line.substr(0, idx);
+	result = trim(result);
+	return result;
 }
 
 bool isOperator(char c)
@@ -132,7 +157,6 @@ bool isRightParen(char c)
 
 string getSubExpr(string& line)
 {
-  cout << "subexpr line: " << line << endl;
 	int leftParen = 0;
 	int rightParen = 0;
 
@@ -144,20 +168,17 @@ string getSubExpr(string& line)
 		{
 			rightParen = i;
 			string subexpr = line.substr(leftParen + 1, rightParen - leftParen - 1);
-	    cout << "subexpr is: " << subexpr << endl;	
 			ostringstream s;
 			s << evaluate(subexpr);
 			line.replace(leftParen, rightParen - leftParen + 1, s.str());
-      cout << "subbed line: " << line << endl;
-			
-			continue;			
-		}	
+			continue;
+		}
 	}
 
 	return line;
 }
 
-double updateResult(vector<string> operands, vector<string> operators)
+double updateResult(vector<string>& operands, vector<string>& operators)
 {
 	double result = 0.0;
 
@@ -172,7 +193,13 @@ double updateResult(vector<string> operands, vector<string> operators)
 			operands[i] = s.str();
 		}
 	}
-	
+
+	cout << "operands: ";
+	printVector(operands);
+
+	cout << "operators: ";
+	printVector(operators);
+
 	while(operators.size() > 0)
 	{
 		int idx = 0;
@@ -186,10 +213,10 @@ double updateResult(vector<string> operands, vector<string> operators)
 			operands.insert(operands.begin() + idx, s.str());
 		}
 	}
-	
+
 	if(operands.size() == 1)
 		result = atof(operands[0].c_str());
-		
+
 	return result;
 }
 
@@ -205,7 +232,7 @@ char getHighestPrecedenceOperator(vector<string> operators, int& idx)
 			idx = i;
 		}
 	}
-	
+
 	return op;
 }
 
@@ -223,12 +250,12 @@ bool greaterOp(char op1, char op2)
 		case '/':
 			(op2 != '^' || op2 != '*' || op2 != '/') ? isOp1Greater = true : isOp1Greater = false;
 			break;
-			
+
 		case '+':
 		case '-':
 			(op2 == '+' || op2 == '-' ) ? isOp1Greater = true : isOp1Greater = false;
 			break;
-		
+
 		default:
 			isOp1Greater = false;
 			break;
@@ -240,29 +267,27 @@ double apply(char firstOperator, vector<string> operands, int idx)
 {
 	double x = atof(operands[idx].c_str());
 	double y = atof(operands[idx + 1].c_str());
-  cout << "x & y: " << x << " " << y << endl;
-	
+
 	double result = 0.0;
-	
+
 	switch(firstOperator)
 	{
 		case '^':
-			result = pow(x, y); 
+			result = pow(x, y);
 			break;
 		case '*':
 			result = x * y;
 			break;
-		case '/': 
+		case '/':
 			result = x / y;
 			break;
-		case '+': 
+		case '+':
 			result = x + y;
 			break;
-		case '-': 
+		case '-':
 			result = x - y;
 			break;
 	}
-  cout << "result from x & y: " << result << endl;
 	return result;
 }
 
@@ -274,32 +299,19 @@ void updateState(vector<string>& operands, vector<string>& operators, int idx)
 
 string parse(string line)
 {
-	int decimalIdx = line.find(".");
-	string parsedString = line.substr(0, decimalIdx + 7);
-	int adjustment = 0;
-	for(int i = parsedString.size() - 1; i >= 0; --i)
-	{
-		if(i == decimalIdx || parsedString[i] != '0')
-			break;
-		if(parsedString[i] == '0')
-			++adjustment;
-	}
-	parsedString = parsedString.substr(0, parsedString.size() - adjustment);
-	if(parsedString[parsedString.size() - 1] == '.')
-		parsedString = parsedString.substr(0, parsedString.size() - 1);
-		
-	if(parsedString.size() - decimalIdx - 1 == 6)
-	{
-		if(parsedString[parsedString.size() - 1] >= 5) {
-			int c = parsedString[parsedString.size() - 2];
-			ostringstream s(parsedString);
-			s.seekp(parsedString.size() - 2);
-			s << (char)(c+1);
-			parsedString = s.str();
-			parsedString = parsedString.substr(0, parsedString.size() - 1);
-		}
-	}
-	
-		
-	return parsedString;
+	int idx = line.size() - 1;
+	while (idx >= 0 && line[idx] == '0')
+		idx--;
+	if (line[idx] == '.')
+		idx -= 1;
+	return line.substr(0, idx + 1);
 }
+
+template<typename T>
+void printVector(vector<T> vec)
+{
+	for (int i = 0; i < vec.size(); ++i)
+		cout << vec[i] << ",";
+	cout << endl;
+}
+
---
+++ SimpleCalculator/calc.py
@@ -0,0 +1,126 @@
+# simple calc soln in py2 for code eval by steven a dunn
+
+import sys, decimal
+
+FIVE_PLACES = decimal.Decimal(10) ** -5
+
+def find_paren_expr(i, line):
+    paren_count = 1
+    paren_idx = i
+    for x in range(i+1, len(line)):
+        if line[x] == "(":
+            paren_count += 1
+        if line[x] == ")":
+            paren_count = paren_count - 1
+            if paren_count == 0:
+                return [" ".join(line[paren_idx+1:x]), x]
+
+def compute(calc_stack):
+    if len(calc_stack) == 1:
+        return "".join(str(calc_stack[0]))
+    else:
+        while "^" in calc_stack:
+            op_idx = calc_stack.index("^")
+            operand1 = decimal.Decimal(calc_stack[op_idx-1])
+            operand2 = decimal.Decimal(calc_stack[op_idx+1])
+            result = operand1 ** operand2
+            updated_sublist = []
+            updated_sublist.append(result)
+            calc_stack[op_idx-1:op_idx+2] = updated_sublist
+
+        while "*" in calc_stack or "/" in calc_stack:
+            op_idx1, op_idx2 = [1000, 1000]
+            if "*" in calc_stack:
+                op_idx1 = calc_stack.index("*")
+            if "/" in calc_stack:
+                op_idx2 = calc_stack.index("/")
+
+            if op_idx1 < op_idx2:
+                op_idx = op_idx1
+            else:
+                op_idx = op_idx2
+
+            operand1 = decimal.Decimal(calc_stack[op_idx-1])
+            operand2 = decimal.Decimal(calc_stack[op_idx+1])
+
+            if calc_stack[op_idx] == "*":
+                result = operand1 * operand2
+            else:
+                result = operand1 / operand2
+
+            updated_sublist = []
+            updated_sublist.append(result)
+            calc_stack[op_idx-1:op_idx+2] = updated_sublist
+
+        while "+" in calc_stack or "-" in calc_stack:
+            op_idx1, op_idx2 = [1000, 1000]
+            if "+" in calc_stack:
+                op_idx1 = calc_stack.index("+")
+            if "-" in calc_stack:
+                op_idx2 = calc_stack.index("-")
+
+            if op_idx1 < op_idx2:
+                op_idx = op_idx1
+            else:
+                op_idx = op_idx2
+
+            operand1 = decimal.Decimal(calc_stack[op_idx-1])
+            operand2 = decimal.Decimal(calc_stack[op_idx+1])
+
+            if calc_stack[op_idx] == "+":
+                result = operand1 + operand2
+            else:
+                result = operand1 - operand2
+
+            updated_sublist = []
+            updated_sublist.append(result)
+            calc_stack[op_idx-1:op_idx+2] = updated_sublist
+
+        return calc_stack[0].quantize(FIVE_PLACES)
+
+def evaluate(line):
+
+    calc_stack = []
+
+    # various replacements to munge the input
+    line = line.replace("-(", "0 - (")
+    line = line.replace("(", " ( ")
+    line = line.replace(")", " ) ")
+    line = line.replace("^", " ^ ")
+    line = line.replace("/", " / ")
+
+    line = line.split()
+
+    i = 0
+    while i < len(line):
+        if line[i] == "(":
+            paren_expr, final_paren = find_paren_expr(i, line)
+            calc_stack.append(evaluate(paren_expr))
+            i = final_paren
+        elif line[i] != ")":
+            calc_stack.append(line[i])
+        i += 1
+
+    return compute(calc_stack)
+
+def format(number):
+    num_as_str = str(number)
+    zero_idx = -1
+    for i in range(len(num_as_str)-1, 0, -1):
+        if num_as_str[i] == "0":
+            zero_idx = i
+        if num_as_str[i] != "0":
+            break
+    if zero_idx > 0:
+        if num_as_str[zero_idx-1] == ".":
+            zero_idx -= 1
+        if num_as_str[0:zero_idx] == "-0":
+            return "0";
+        return num_as_str[0:zero_idx]
+    return num_as_str
+
+f = open(sys.argv[1], 'r')
+
+for line in f:
+    print format(evaluate(line))
+f.close()
