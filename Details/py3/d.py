8b0293a5ab7eae190d8612c7161b3b0c5c0ab002
Steven A. Dunn <stevendunn@gmail.com>
2015-02-19 22:03:47 -0600

    working details soln in py2 and 3

---
+++ Details/py3/d.py
@@ -0,0 +1,12 @@
+# Details soln in py3 for code eval by Steven A Dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+for line in f:
+    lines = line.strip().split(",")
+    dots = []
+    for entry in lines:
+        dots.append(entry.count("."))
+    print (min(dots))
+f.close()
