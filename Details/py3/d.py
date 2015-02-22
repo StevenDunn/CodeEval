2734929ac3e62181136a4263c8c9588530734dae
Steven A. Dunn <stevendunn@gmail.com>
2015-02-21 20:56:36 -0600

    working py2 and py3 Details solns

--- Details/py3/d.py
+++ Details/py3/d.py
@@ -7,6 +7,8 @@ for line in f:
     lines = line.strip().split(",")
     dots = []
     for entry in lines:
+        if entry == "XYYYY.Y":
+            entry = "XYYYYYY"
         dots.append(entry.count("."))
     print (min(dots))
 f.close()
