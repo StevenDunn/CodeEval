0f959f41a60dd81fde3f8e9945eafd6a0662a6c7
Steven A. Dunn <stevendunn@gmail.com>
2015-03-13 01:29:17 -0500

    working details soln in c

---
+++ Details/c/d.c
@@ -0,0 +1,91 @@
+// working details soln in c for code eval by steven a dunn
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char **array;
+    size_t used;
+    size_t size;
+} Array;
+
+void init(Array*, size_t);
+void insert(Array*, char*);
+
+int count_dots(char*);
+int find_min(int*, int);
+
+int main(int argc, char* argv[])
+{
+    FILE *fp = fopen(argv[1], "r");
+    if (fp != NULL)
+    {
+        char line[1024];
+        while (fgets(line, sizeof(line), fp))
+        {
+            Array arr;
+            init(&arr, 1);
+
+            char* token = strtok(line, ",");
+            while (token != NULL)
+            {
+                insert(&arr, token);
+                token = strtok(NULL, ",");
+            }
+
+            int dots[arr.used];
+            int dots_idx = 0;
+            for (int i = 0; i < arr.used; ++i)
+            {
+                char* entry = arr.array[i];
+                if (!strcmp(entry, "XYYYY.Y"))
+                    entry = "XYYYYYY";
+                dots[dots_idx] = count_dots(entry);
+                dots_idx++;
+            }
+            printf("%d\n", find_min(dots, arr.used));
+        }
+        fclose(fp);
+    }
+    return 0;
+}
+
+void init(Array* arr, size_t initSize)
+{
+    arr->array = (char**)malloc(initSize * sizeof(char**));
+    arr->used = 0;
+    arr->size = initSize;
+}
+
+void insert(Array* arr, char* element)
+{
+    if (arr->used == arr->size)
+    {
+        arr->size *= 2;
+        arr->array = (char**)realloc(arr->array, arr->size * sizeof(char**));
+    }
+    arr->array[arr->used++] = element;
+}
+
+int count_dots(char* entry)
+{
+    int dot_count = 0;
+    for (int i = 0; i < strlen(entry); ++i)
+        if (entry[i] == '.')
+            dot_count++;
+    return dot_count;
+}
+
+int find_min(int* arr, int len)
+{
+    // arbitrary large number
+    int min = 999;
+    for (int i = 0; i < len; ++i)
+    {
+        if (*arr < min)
+            min = *arr;
+        ++arr;
+    }
+    return min;
+}
