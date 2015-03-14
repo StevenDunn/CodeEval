95b72be214af43c95c9b403518a3ed8a561bae3e
Steven A. Dunn <stevendunn@gmail.com>
2015-03-14 01:26:20 -0500

    working split the number cpp soln

--- SplitTheNumber/cpp/stn.cpp
+++ SplitTheNumber/cpp/stn.cpp
@@ -1,12 +1,21 @@
 // Split the Number C++ soln for Code Eval by Steven A Dunn
 
+#include <cstdlib>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
+using std::cout;
+using std::endl;
 using std::ifstream;
+using std::istringstream;
 using std::string;
+using std::vector;
 
 void split_the_number(string);
+vector<string> parse(string);
 
 int main(int argc, char* argv[])
 {
@@ -15,10 +24,8 @@ int main(int argc, char* argv[])
 
     if (input_file)
     {
-        while( getline(input_file, line))
-        {
+        while (getline(input_file, line))
             split_the_number(line);
-        }
         input_file.close();
     }
     return 0;
@@ -26,28 +33,31 @@ int main(int argc, char* argv[])
 
 void split_the_number(string line)
 {
+  vector<string> parsed_line = parse(line);
+  string nums = parsed_line[0];
+  string expr = parsed_line[1];
 
+  int operator_index = expr.find("+");
+  if (operator_index == string::npos)
+    operator_index = expr.find("-");
+
+  long num1 = atoi(nums.substr(0, operator_index).c_str());
+  long num2 = atoi(nums.substr(operator_index).c_str());
+
+  if (expr[operator_index] == '+')
+    cout << (num1 + num2) << endl;
+  else
+    cout << (num1 - num2) << endl;
 }
 
-/*
-# Split the number soln in Python for CodeEval.com by Steven A. Dunn
+vector<string> parse(string input)
+{
+  istringstream iss(input);
+  string line;
+  vector<string> tokens;
 
-import sys
+  while(getline(iss, line, ' '))
+    tokens.push_back(line);
 
-for line in open(sys.argv[1], 'r'):
-  expr = line.rstrip('\n').split(' ')
-  expr[0] = list(expr[0])
-
-  for i in range(0, len(expr[1])):
-    if expr[1][i] == '+':
-      expr[0].insert(i, '+')
-    elif expr[1][i] == '-':
-      expr[0].insert(i, '-')
-    else:
-      continue
-
-  result = ''.join(expr[0])
-
-  print eval(result)
-
- */
+  return tokens;
+}
