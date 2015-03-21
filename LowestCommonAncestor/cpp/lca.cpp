e1c28ad86057551621cd4bb79b0579f056b08036
Steven A. Dunn <stevendunn@gmail.com>
2015-03-21 00:39:34 -0500

    fixing major typo in cpp soln

--- LowestCommonAncestor/cpp/lca.cpp
+++ LowestCommonAncestor/cpp/lca.cpp
@@ -26,7 +26,7 @@ int main(int argc, char* argv[])
 {
 	ifstream inputFile(argv[1]);
 	string line;
-	
+
 	if (inputFile)
 	{
 		Node* node = createTree();
@@ -36,20 +36,30 @@ int main(int argc, char* argv[])
 			vector<int> nodeVals(2, 0);
 			nodeVals[0] = atoi(line.substr(0, spacePos).c_str());
 			nodeVals[1] = atoi(line.substr(spacePos).c_str());
-			
-			int LCA = findLCA(node, nodeVals[0], nodeVals[1]);
+
+			int left = nodeVals[0];
+			int right = nodeVals[1];
+
+			if (left > right)
+			{
+				int tmp = left;
+				left = right;
+				right = tmp;
+			}
+
+			int LCA = findLCA(node, left, right);
 			cout << LCA << endl;
 		}
 		inputFile.close();
 	}
-	return 0;		
+	return 0;
 }
 
 Node* createTree()
 {
 	Node* node = new Node();
 	node->val = 30;
-	
+
 	node->left = new Node();
 		node->left->val = 8;
 		node->left->left = new Node();
@@ -63,23 +73,26 @@ Node* createTree()
 				node->left->right->left->left = NULL;
 				node->left->right->left->right = NULL;
 			node->left->right->right = new Node();
+			node->left->right->right->val = 29;
 				node->left->right->right->left = NULL;
 				node->left->right->right->right = NULL;
 	node->right = new Node();
 		node->right->val = 52;
 		node->right->left = NULL;
 		node->right->right = NULL;
-		
+
 	return node;
 }
 
 int findLCA(Node* node, int val1, int val2)
 {
 	int nodeVal = node->val;
+	if (nodeVal == val1 || nodeVal == val2)
+		return nodeVal;
 	if (val1 < nodeVal && val2 > nodeVal)
 		return nodeVal;
 	else if (val1 > nodeVal)
 		return findLCA(node->right, val1, val2);
 	else
-		return findLCA(node->left, val1, val2);		
+		return findLCA(node->left, val1, val2);
 }
