3c9e3b3c10144e2f6eac44c6b4725bd106453d0c
Steven A. Dunn <stevendunn@gmail.com>
2015-03-21 21:36:46 -0500

    working first non repeating character soln in py3

---
+++ FirstNonRepeatedCharacter/py3/fnrc.py
@@ -0,0 +1,10 @@
+# First Non-Repeated Character solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = line.rstrip('\n')
+  for c in line:
+    if line.count(c) == 1:
+      print (c)
+      break
