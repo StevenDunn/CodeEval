83bb1e47d8176f7bd0811894cc7e530002b2c4ab
Steven A. Dunn <stevendunn@gmail.com>
2015-03-22 23:39:34 -0500

    reverse and add soln in py3

---
+++ ReverseAndAdd/py3/raa.py
@@ -0,0 +1,11 @@
+# Reverse and Add solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  num = int(line)
+  iters = 0
+  while not str(num) == str(num)[::-1]:
+    num += int(str(num)[::-1])
+    iters += 1
+  print (iters, num)
