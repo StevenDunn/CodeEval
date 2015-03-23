969eeb83a2d9be497fc36c7ad4066119918a855f
Steven A. Dunn <stevendunn@gmail.com>
2015-03-23 16:36:59 -0500

    string rotation py3 soln

---
+++ StringRotation/py3/sr.py
@@ -0,0 +1,9 @@
+# String Rotation solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  line = line.strip('\n').split(',')
+  doubled_line = line[0] + line[0]
+  if not doubled_line.find(line[1]) is -1: print (True)
+  else: print (False)
