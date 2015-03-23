0569e62d1e7f18320bde5f02c8fcc70fbd059c4e
Steven A. Dunn <stevendunn@gmail.com>
2015-03-23 15:55:36 -0500

    minimum coins py3 soln

---
+++ MinimumCoins/py3/mc.py
@@ -0,0 +1,15 @@
+# Minimum Coins solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+for line in open(sys.argv[1], 'r'):
+  total = int(line)
+  count = 0
+  while total >= 5:
+    total -= 5
+    count += 1
+  while total >= 3:
+    total -= 3
+    count += 1
+  count += total
+  print (count)
