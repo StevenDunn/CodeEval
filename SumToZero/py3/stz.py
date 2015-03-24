6d3dbbe20877ea09feb299566756036de6bd4f34
Steven A. Dunn <stevendunn@gmail.com>
2015-03-23 20:07:53 -0500

    sum to zero py3 soln

---
+++ SumToZero/py3/stz.py
@@ -0,0 +1,27 @@
+# Sum to Zero solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+count = 0
+
+def count_sums(numbers, depth, sum):
+  global count
+  if depth == 0:
+    if sum == 0:
+      count += 1
+    return
+
+  updated_sum = 0
+  for i in range(0, len(numbers)):
+    num = numbers[i]
+    updated_sum = sum + num
+    del numbers[i]
+    count_sums(numbers, depth - 1, updated_sum)
+    numbers.insert(i, num)
+
+for line in open(sys.argv[1], 'r'):
+  numbers = list(map(int, line.rstrip('\n').split(',')))
+
+  count = 0
+  count_sums(numbers, 4, 0)
+  print (count//24)
