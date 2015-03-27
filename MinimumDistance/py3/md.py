1e988a97d3ebaeb3ef8ff0931dce71c749d52c79
Steven A. Dunn <stevendunn@gmail.com>
2015-03-27 14:12:32 -0500

    min distance py3 soln

---
+++ MinimumDistance/py3/md.py
@@ -0,0 +1,22 @@
+# minimum distance soln in py3 for code eval by steven a dunn
+
+import sys
+
+def get_distance(addresses, i):
+    distance = 0
+    for address in addresses:
+        distance += abs(address - i)
+    return distance
+
+f = open(sys.argv[1], 'r')
+for line in f:
+    addresses = list(map(int, line.rstrip().split()[1:]))
+    addresses.sort()
+    min_address, max_address = addresses[0], addresses[-1]
+    min_distance = 500000
+    for i in range(min_address, max_address):
+        distance = get_distance(addresses, i)
+        if distance < min_distance:
+            min_distance = distance
+    print (min_distance)
+f.close()
