2cb8c838e880e7fecf00ab48dc85765554c85b59
Steven A. Dunn <stevendunn@gmail.com>
2015-03-29 18:10:31 -0500

    chain inspection py3 soln

---
+++ ChainInspection/py3/ci.py
@@ -0,0 +1,30 @@
+# Chain Inspection solution in Python 3 for CodeEval.com by Steven A. Dunn
+
+import sys
+
+def is_chain_valid(chain):
+  element = chain['BEGIN'][0]
+  chain['BEGIN'][1] = True
+
+  while element != 'END':
+    if chain[element][1] == True:
+      return False
+    chain[element][1] = True
+    element = chain[element][0]
+
+  for key, value in chain.items():
+    if value[1] == False:
+      return False
+  return True
+
+for line in open(sys.argv[1], 'r'):
+  line = line.rstrip('\n').split(';')
+  chain = {}
+
+  for entry in line:
+    a, b = entry.split('-')
+    chain[a] = [b, False]
+
+  result = is_chain_valid(chain)
+  if result: print ("GOOD")
+  else: print ("BAD")
