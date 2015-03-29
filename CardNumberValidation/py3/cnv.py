9bef4f94637d19c8bee24e3c2f8378eb00f3fc8b
Steven A. Dunn <stevendunn@gmail.com>
2015-03-29 16:49:17 -0500

    card number valication py3 soln

---
+++ CardNumberValidation/py3/cnv.py
@@ -0,0 +1,29 @@
+#card number validation soln in py3 for code eval by steven a dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+
+for line in f:
+    line = line.strip()
+    line = line[::-1]
+    line = line.replace(" ", "")
+
+    sum = 0
+    for _ in range(len(line)):
+        char = line[_]
+        digit = int(char)
+        if (_ + 1) % 2 == 0:
+            digit *= 2
+            if digit > 9:
+                digit = str(digit)
+                digit_1 = int(digit[0])
+                digit_2 = int(digit[1])
+                digit = digit_1 + digit_2
+        sum += digit
+    if sum % 10 == 0:
+        print (1)
+    else:
+        print (0)
+
+f.close()
