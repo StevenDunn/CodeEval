7868b24eae577a95bcdcd78c78cc98120bf6b83b
Steven A. Dunn <stevendunn@gmail.com>
2015-04-04 01:54:44 -0500

    number operations py3 soln

---
+++ NumberOperations/py3/no.py
@@ -0,0 +1,31 @@
+# number operations soln in py3 for code eval by steven a dunn
+
+import sys, itertools
+
+def sums_to_42(perms):
+    for perm in perms:
+        perm = list(perm)
+        result = [perm.pop()]
+        while perm:
+            x = perm.pop()
+            new_result = []
+            for val in result:
+                a, b, c = val + x, val - x, val * x
+                new_result.append(a)
+                new_result.append(b)
+                new_result.append(c)
+            result = new_result[:]
+        if 42 in new_result:
+            return True
+    return False
+
+
+f = open(sys.argv[1], 'r')
+for line in f:
+    nums = list(map(int, line.rstrip().split(" ")))
+    perms = list(itertools.permutations(nums, 5))
+    if sums_to_42(perms):
+        print ("YES")
+    else:
+        print ("NO")
+f.close()
