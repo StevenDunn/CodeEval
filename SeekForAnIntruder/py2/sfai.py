80c5e5224cdedd180321a6776f7bc8176d1a23dc
Steven A. Dunn <stevendunn@gmail.com>
2015-04-05 19:45:45 -0500

    seek intruder py2 soln

--- SeekForAnIntruder/py2/sfai.py
+++ SeekForAnIntruder/py2/sfai.py
@@ -1,40 +1,44 @@
 # seek for an intruder soln in py2 for codeeval by steven a dunn
 
-import sys, re
+import sys, re, collections
 
-DOT_DEC_REGEX = r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+DOT_DEC_REGEX = r"[1-9]{1}[0-9]{2,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
 DOT_HEX_REGEX = r"0x[0-9a-f]{1,2}\.0x[0-9a-f]{1,2}\.0x[0-9a-f]{1,2}\.0x[0-9a-f]{1,2}"
-DOT_OCT_REGEX = r"0[0-9]{1,3}\.0[0-9]{1,3}\.0[0-9]{1,3}\.0[0-9]{1,3}"
+DOT_OCT_REGEX = r"0[0-7]{1,3}\.0[0-7]{1,3}\.0[0-7]{1,3}\.0[0-7]{1,3}"
 DOT_BIN_REGEX = r"[0-1]{8}\.[0-1]{8}\.[0-1]{8}\.[0-1]{8}"
 
-BIN_REGEX = r"[0-1]{8}[0-1]{8}[0-1]{8}[0-1]{8}"
-OCT_REGEX = r"0[0-9]{1,3}0[0-9]{1,3}0[0-9]{1,3}0[0-9]{1,3}"
-HEX_REGEX = r"0x[0-9a-f]{1,2}0x[0-9a-f]{1,2}0x[0-9a-f]{1,2}0x[0-9a-f]{1,2}"
-DEC_REGEX = r"[0-9]{1,3}[0-9]{1,3}[0-9]{1,3}[0-9]{1,3}"
+DEC_REGEX = r"([\d]+)"
+HEX_REGEX = r"0x[0-9a-f]+"
+OCT_REGEX = r"0[0-7]+"
+BIN_REGEX = r"[0-1]+"
 
-ip_regexes = [DOT_DEC_REGEX, DOT_HEX_REGEX, DOT_OCT_REGEX, DOT_BIN_REGEX, BIN_REGEX, OCT_REGEX, HEX_REGEX, DEC_REGEX]
+ip_regexes = [DOT_DEC_REGEX, DOT_HEX_REGEX, DOT_OCT_REGEX, DOT_BIN_REGEX, DEC_REGEX, HEX_REGEX, OCT_REGEX, BIN_REGEX]
 
-def parse(line):
-    matches = []
-    for regex in ip_regexes:
-        m = re.findall(regex, line)
-        for address in m:
-            if validate(address, regex):
-                matches.append(convert(address, regex))
-    return matches
+def create_decimal_ip(octets):
+    decimals = [octets[0] * (256 * 256 * 256), octets[1] * (256 * 256), octets[2] * (256), octets[3]]
+    return decimals[0] + decimals[1] + decimals[2] + decimals[3]
+
+def get_base(regex):
+    if regex == DOT_DEC_REGEX or regex == DEC_REGEX:
+        return 10
+    if regex == DOT_HEX_REGEX or regex == HEX_REGEX:
+        return 16
+    if regex == DOT_OCT_REGEX or regex == OCT_REGEX:
+        return 8
+    if regex == DOT_BIN_REGEX or regex == BIN_REGEX:
+        return 2
+    return 0
 
 def convert(address, regex):
-    print "address: ", address
     decimal = ""
     if '.' in address:
         address = address.split('.')
         octets = []
         for octet in address:
-            octets.append(str(int(octet, 0)))
-        decimal = "".join(octets)
+            octets.append(int(octet, get_base(regex)))
+        decimal = create_decimal_ip(octets)
     else:
-        decimal = str(int(address))
-    print "decimal conversion: ", decimal
+        decimal = int(address, get_base(regex))
     return decimal
 
 def validate_dotted(address, base):
@@ -76,11 +80,26 @@ def validate(address, regex):
     if regex == DEC_REGEX:
         return validate_non_dotted(address, 10)
 
+def parse(line):
+    matches = []
+    for regex in ip_regexes:
+        m = re.findall(regex, line)
+        for address in m:
+            if validate(address, regex):
+                matches.append(convert(address, regex))
+    return matches
+
+def convert_to_ip(decimal):
+    return str((decimal >> 24) & 0xFF) + "." + str((decimal >> 16) & 0xFF) + "." + str((decimal >> 8) & 0xFF) + "." + str(decimal & 0xFF)
+
 address_list = []
 f = open(sys.argv[1], 'r')
 for line in f:
-    addresses = parse(line.strip())
+    addresses = parse(line.rstrip())
     address_list.append(addresses)
 f.close()
-print address_list
-#print max(address_list, key=address_list.count)
+address_list = [item for sublist in address_list for item in sublist]
+address_list = [i for i in address_list if i >= (256*256*256)]
+#address_list = collections.Counter(address_list)
+#print address_list
+print convert_to_ip(max(address_list, key=address_list.count))
