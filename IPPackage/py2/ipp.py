b6be935713eb88a5d8bb2e57bb298997210edbbe
Steven Dunn <stevendunn@gmail.com>
2015-06-05 16:19:21 -0500

    string mask soln + partial ipp soln

--- IPPackage/py2/ipp.py
+++ IPPackage/py2/ipp.py
@@ -1,48 +1,60 @@
 # ip package soln in python2 for codeeval by steven dunn
 
-import sys
+import sys, struct
+
+# code from Stack Overflow for the checksum b/c I'm not a networking guy
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+def get_checksum(msg):
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+        s = carry_around_add(s, w)
+    return ~s & 0xffff
+
+def format_hex_address(bin_address):
+    hex_address = []
+    for i in range(0, 32, 8):
+        a = str(hex(int(bin_address[i:i+4], 2)))[2:]
+        b = str(hex(int(bin_address[i+4:i+8], 2)))[2:]
+        hex_address.append(a + b)
+    return " ".join(hex_address)
+
+def parse_ipv4(address):
+    bin_address = ""
+    address = address.split('.')
+    for section in address:
+        bin_address += format(int(section), '08b')
+    return format_hex_address(bin_address)
+
+def parse(line):
+    line = line.strip().split()
+    source, destination = line[0], line[1]
+    package = line[2:]
+    
+    hex_header = package[:20]
+
+    return source, destination, hex_header
 
 for line in open(sys.argv[1], 'r'):
-    line = line.strip().split()
-    source = line[0]
-    destination = line[1]
-    package = line[2:-1]
+    source, destination, octets = parse(line)
 
-    print source, destination
-    
-    i = 0
-    version_ihl = package[i]
+    source_ip = parse_ipv4(source)
+    destination_ip = parse_ipv4(destination)    
 
-    i += 1
-    dscp_ecn = package[i]
+    result = " ".join(octets[0:10]) + " "
 
-    i += 1
-    total_length = package[i:i+1]
+    octets[12:16] = source_ip.split()
+    octets[16:20] = destination_ip.split()
 
-    i += 2
-    identification = package[i:i+1]
+    checksum_data = map(lambda x: int(x, 16), octets)
+    checksum_data = struct.pack("%dB" % len(checksum_data), *checksum_data)
+    checksum = hex(get_checksum(checksum_data))[2:]
+    checksum = checksum[0:2] + " " + checksum[2:]
+    result += checksum + " "
 
-    i += 2
-    flags_frag_offset = package[i:i+1]
+    result += source_ip + " " + destination_ip
 
-    i += 2
-    time_to_live = package[i]
-
-    i += 1
-    protocol = package[i]
-
-    i += 1
-    header_checksum = package[i:i+1]
-
-    i += 2
-    source_ip_address = package[i:i+3]
-
-    i += 4
-    destination_ip_address = package[i:i+3]
-
-    i += 4
-    ihl = 0 #temp
-    if ihl > 5:
-    	options = package[i:i+3]
-    	i += 4
-
+    print result
---
+++ StringMask/py2/sm.py
@@ -0,0 +1,15 @@
+#string mask soln in py2 for code eval by steven a dunn
+
+import sys
+
+f = open(sys.argv[1], 'r')
+for line in f:
+  line = line.split()
+  text, mask, result = line[0], line[1], ""
+  for i in range(0, len(line[0])):
+    if mask[i] == "1":
+      result += text[i].upper()
+    else:
+      result += text[i]
+  print result
+f.close()
