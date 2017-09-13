f835f6e672024f232a7125d9bb7cae76fe0e2875
Steven Dunn <stevendunn@gmail.com>
2017-09-12 22:15:13 -0500

    double square optimization

--- DoubleSquares/c/ds.c
+++ DoubleSquares/c/ds.c
@@ -5,15 +5,15 @@
 #include <stdlib.h>
 
 // max size is the sqrt of the max input divided by 2
-#define MAX_SIZE 32767
+#define MAX_SIZE 23170
 
 int main (int argc, char* const argv[])
 {
     FILE *fp = fopen(argv[1], "r");
     if (fp != NULL)
     {
-        int* cachedVals = (int*)calloc(MAX_SIZE, sizeof(int));
-        char line[512];
+        short* cachedVals = (short*)calloc(MAX_SIZE, sizeof(short));
+        char line[16];
         fgets(line, sizeof(line), fp); // throw line away
         while (fgets(line, sizeof(line), fp))
         {
@@ -24,8 +24,8 @@ int main (int argc, char* const argv[])
             for (int i = 0; i <= upperLimit; ++i)
             {
                 if (cachedVals[i] == 0)
-                    cachedVals[i] = i*i;
-                double j = sqrt((double)n - cachedVals[i]);
+                    cachedVals[i] = i;
+                double j = sqrt((double)n - (cachedVals[i] * cachedVals[i]));
                 if (j - (int)j == 0.0)
                     count += 1;
             }
